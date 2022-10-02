e95ca6ae336ffc9de4b8df9600c907770b95f225
Austin <austingwatson@hotmail.com>
2022-10-01 20:31:47 -0500

    changed window size

--- shooter/HUD.gd
+++ shooter/HUD.gd
@@ -4,15 +4,20 @@ extends Control
 onready var ten_second_timer = $TenSecondTimer
 
 export (Texture) var health_texture
-const health_x_offset = 50
-var health_x = 10
-const health_y = 500
+const health_y_offset = 50
+const health_x = 10
+var health_y = 0
 var player_health = 10
 
+func _ready():
+	health_y = get_viewport_rect().size.y - 42
+
 func _draw():
+	var temp_health_y = health_y
 	for i in player_health:
 		draw_texture_rect(health_texture, Rect2(health_x, health_y, 32, 32), false)
-		health_x += health_x_offset
+		health_y -= health_y_offset
+	health_y = temp_health_y
 
 func update_ten_second_timer(time):
 	ten_second_timer.text = str(stepify(time, 1))
