cab2f45aeae0110991c2d85d035ab5a10bf09eb5
Austin <austingwatson@hotmail.com>
2022-10-12 01:15:09 -0500

    added limit to player health rendering

--- shooter/hud/PlayerHealth.gd
+++ shooter/hud/PlayerHealth.gd
@@ -10,16 +10,16 @@ var player_health = 10
 
 func _ready():
 	health_y = get_viewport_rect().size.y - health_size - 180
-	
+
 func _draw():
 	var temp_health_y = health_y
 	for i in player_health:
 		draw_texture_rect(health_texture, Rect2(health_x, health_y, health_size, health_size), false)
 		health_y -= health_y_offset
 	health_y = temp_health_y
-	
+
 func update_player_health(health):
-	if health > 40:
-		health = 40
+	if health > 30:
+		health = 30
 	player_health = health
 	update()
